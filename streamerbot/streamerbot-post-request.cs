570c0ea7d5b20757267e7a6d82a9c601fe246313
Thorsten Fieger <Thorsten.Fieger@gmail.com>
2022-11-10 20:59:48 +0100

    Format

--- streamerbot/streamerbot-post-request.cs
+++ streamerbot/streamerbot-post-request.cs
@@ -43,7 +43,7 @@ public class CPHInline
     public bool Execute()
     {
         sendCommand(left, "green");
-	sendCommand(right, "yellow");
+	    sendCommand(right, "yellow");
 	return true;
     }
 }
